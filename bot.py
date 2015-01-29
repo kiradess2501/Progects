4d35044368fb4be2eb43a4f0215284ba6def9880
root <root@Dixy.(none)>
2015-01-29 12:24:22 -0800

    added comments to the bot

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ by /u/NEET_Here
 
 r.login()
 
+# List of words the bot will reply to
 words_to_match = ['hey', 'hello', 'hi', 'sup', 'greetings',
 				'howdy', 'bonjour','good day', 'good morning', 
 				'hiya', 'how goes it', 'whats up',
@@ -20,19 +21,30 @@ words_to_match = ['hey', 'hello', 'hi', 'sup', 'greetings',
 
 cache = []
 
+# Function to run the bot
 def run_bot():
 	subreddit = r.get_subreddit('test')
 	comments = subreddit.get_comments(limit=25)
+	
 	for comment in comments:
 		comment_text = comment.body.lower()
+		
+		# Bot was replying to any words that contained the string
+		# regardless of whether it was singular
+		# So I'm trying to split them
 		comment_text = comment_text.split(' ')
+		
 		for word in words_to_match:
 			if comment.id not in cache and word in comment_text:
 				print ('Replying...')
 				comment.reply('This is a test bot. Just sayin\' {0}'.format(word))
 				cache.append(comment.id)
 				time.sleep(2)
-			
-while True:
-	run_bot()
-	time.sleep(10)
+
+def main():		
+	while True:
+		run_bot()
+		time.sleep(10)
+
+if __name__ = '__main__':
+	main()
