81e3195b28ff92245fbd0b65e3fe56fb539c3e23
Tristan Smith <tristanplaysguitar@gmail.com>
2015-01-29 16:19:49 -0800

    Added prompt to get username from user. This is used later when checking whether the bot wrote a specified comment. If the bot wrote it, it will ignore the comment. This prevents the endless loop of a bot responding to itself.

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ import time
 r = praw.Reddit(user_agent = "Test bot for /r/progects by /u/NEET_Here and /u/triple-take")
 
 print("Logging in...")
-r.login()
+userN = input("Username: ")
+r.login(user=userN)
 
 # List of words the bot will reply to
 words_to_match = ['hey', 'hello', 'hi', 'sup', 'greetings',
@@ -16,30 +17,31 @@ print("Successfull")
 
 # Function to run the bot
 def run_bot():
-	# Cache file where comment Id's are stored
-	f = open("commentIDcache.txt", "+r")
-	
-	print("Grabbing subreddit...")
-	subreddit = r.get_subreddit("test")
-	comments = subreddit.get_comments(limit=25)
+    # Cache file where comment Id's are stored
+    f = open("commentIDcache.txt", "+r")
 
-	for comment in comments:
-		print("Reading comment...")
-		comment_text = comment.body.lower().split(' ')
+    print("Grabbing subreddit...")
+    subreddit = r.get_subreddit("test")
+    comments = subreddit.get_comments(limit=25)
+    
+    for comment in comments:
+        print("Reading comment...")
+	comment_text = comment.body.lower().split(' ')
 		
-		for commentWord in comment_text:
-				for word in words_to_match:
-					if word == commentWord and comment.id not in f.read():
-						print("Comment found, ID: " + comment.id)
-						print ('Replying...')
-						comment.reply("This is a test bot. Just sayin' hey")
-						print ('Writing Comment ID to Cache')
-						f.write(comment.id + "\n")
-						print ('Cache Updated')
-	time.sleep(10)
-	f.close()
+	for commentWord in comment_text:
+	    for word in words_to_match:
+                if word == commentWord and comment.id not in f.read() and comment.author != r.get_redditor(user=userN):
+		    print("Comment found, ID: " + comment.id)
+                    print ('Replying...')
+                    comment.reply("This is a test bot. Just sayin' hey")
+                    print ('Writing Comment ID to Cache')
+                    f.write(comment.id + "\n")
+                    print ('Cache Updated')
+    f.close()
 
 def main():		
-	while True:
-		run_bot()
+    while True:
+        run_bot()
+        time.sleep(100)
+
 main()
